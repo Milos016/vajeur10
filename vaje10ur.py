8e747f095841abcc773d9e452d38a443d742d1df
Milos016 <71767697+Milos016@users.noreply.github.com>
2020-12-15 17:03:56 +0100

    Update vaje10ur.py
    novoime = input('Ime: ' ) # zdaj input dela ker se išče od uporabnika da vpiše ime
    print('Welcome ' + i)
    while a > 0:  #tuki ne sme biti neskončna zanka, ker se program ne bo ustavil, spremenljivka a je vedno veča kot 0
    a -= a boljše da se a manjša ker potem ni neskončna, mogoče bolj smisleno

--- vaje10ur.py
+++ vaje10ur.py
@@ -1,12 +1,10 @@
-a = 1
-b = 3
-
-def ime(i):
-    input('Ime: '+i)
-    return ('Welcome' +i)
-
-while a > 0:
+    a = 1
+    b = 3
+#Mogoče boljše brez funkcije izdelati algoritem
+    i = input('Ime: ') # tukaj input nič ne dela
+    print('Welcome ' + i)
+    while a > 0:  #tuki ne sme biti neskončna zanka, ker se program ne bo ustavil, spremenljivka a je vedno veča kot 0
     b += a
-    a += a
-    print(b)
+    a -= a  #tuki minus namesto plus spremeni
+    print(b)` #
 
